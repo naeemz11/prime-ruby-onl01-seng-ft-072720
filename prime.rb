e8d83fd16bb9c70abc2e94a2d1387ed159768fc4
Zannatul Naeem <mollahnaeem@gmail.com>
2020-08-03 21:01:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
+def prime?(integer)
+  start = 2
+  if integer > 1
+    range = (start..integer-1).to_a
+    range.none? do |num_to_test|  
+                                
+      integer % num_to_test == 0
+    end
+  else
+    false
+  end
+end
 
